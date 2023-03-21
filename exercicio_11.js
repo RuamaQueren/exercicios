ea5abefed42c8bed780265fb5969a64c1efdc6e4
Ruama Almeida <83236489+RuamaQueren@users.noreply.github.com>
2023-03-20 23:27:02 -0300

    Add files via upload

---
+++ exercicio_11.js
@@ -0,0 +1,47 @@
+/*Escreva um programa que imprime os números de 1 a 10 na tela:*/
+
+let numeros= 1;
+do {
+    console.log (numeros)
+    numeros++
+} while (numeros<=10);
+console.log("numeros")
+
+/*Crie um algoritmo que escreva todos os números entre dois números específicos*/
+
+let controle = 1;
+do {
+    console.log (controle)
+    controle++
+} while (controle<=10);
+console.log ("controle")
+
+
+/*Crie um loop que imprima os números pares de 0 a 20:*/
+
+let par = 0;
+do {
+    console.log (par)
+    par +=2
+} while (par<=20);
+console.log ("par")
+
+/*Crie um algoritmo que escreva a soma de todos os números de 1 a 100*/
+
+let numero1 = 1;
+let soma = 0;
+do {
+    console.log (soma)
+    numero1++
+    soma += numero1;
+} while (numero1<=100)
+console.log ("soma")
+
+/*Crie um algoritmo que conte de 100 até 0*/
+
+let numero = 100;
+do {
+    console.log (numero)
+    numero --
+} while (numero>=0)
+console.log ("numero")
